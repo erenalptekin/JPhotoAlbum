0617e4b5425d481906c6862711b0ff4806b229aa
Eren <Eren@sibel-ub>
2017-01-18 13:09:36 +0300

    a

--- tests/fi/iki/jka/JPhotoFrameTest.java
+++ tests/fi/iki/jka/JPhotoFrameTest.java
@@ -1,13 +1,27 @@
 package fi.iki.jka;
 
+import org.exolab.castor.types.Date;
+import org.exolab.castor.types.DateTimeBase;
+import org.exolab.castor.xml.validators.DateTimeValidator;
 import org.junit.Test;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import fi.iki.jka.JPhotoFrame.*;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class JPhotoFrameTest {
     @Test
-    public void placeholder() throws Exception {
-        assertThat(2, equalTo(2));
+    public void StartSlideshow() throws Exception {
+        final ActionEvent event = new ActionEvent("", 1, "Start Slideshow", System.currentTimeMillis(), 0);
+
+
+        JPhotoFrame frame = new JPhotoFrame("frame", new String[]{"123"});
+        frame.actionPerformed(event);
+
     }
 }
